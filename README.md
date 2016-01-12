0e33284c75e183944d9dd978ff4e78d224680ce4
Sydney Moutia <sydney@akhann.com>
2016-01-12 22:57:14 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+product-demo-server
+===================
+
+A Symfony project created on January 12, 2016, 10:20 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /*
+     * Getter for id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
